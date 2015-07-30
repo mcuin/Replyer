5cc74bbd137254ebb14d7d3ad7d9a2fc62cbc7eb
Mykal Cuin <mykcuin@gmail.com>
2015-07-30 11:10:21 -0600

    Started to work on the email feedback page. Added 2 edit feilds and a button and intilaized them.

--- TextBack/app/src/main/java/com/mykal/textback/Feedback.java
+++ TextBack/app/src/main/java/com/mykal/textback/Feedback.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Feedback extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class Feedback extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        EditText emailSubject = (EditText) findViewById(R.id.emailSubject);
+        EditText emailMessage = (EditText) findViewById(R.id.emailMessage);
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+
+
     }
 }
